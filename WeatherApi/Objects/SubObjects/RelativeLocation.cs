b394062fcac3dbe2cb5c5a4e52bab5985399fb80
Jrdiver <jrdiver@users.noreply.github.com>
2021-07-10 19:57:46 -0500

    finish slitting out sub objects

---
+++ WeatherApi/Objects/SubObjects/RelativeLocation.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherApi.Objects.SubObjects
+{
+    public class RelativeLocation
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonProperty("properties")]
+        public CityInfo Properties { get; set; }
+    }
+}
